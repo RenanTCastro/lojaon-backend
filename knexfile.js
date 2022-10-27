269285b97e134c6182f18b8c8718217b8eaa388f
RenanTCastro <renancastrotut@gmail.com>
2022-10-27 17:31:42 -0300

    ajustes para backend remoto

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: 'postgres://ndkqluawihosqf:7051f1f32e90613480c4856a75f98bc88752ee601485bb6f0a1df1115048eeb1@ec2-3-230-122-20.compute-1.amazonaws.com:5432/d3d48bbuuqsogn',
       ssl: { rejectUnauthorized: false },
     },
     migrations: {
